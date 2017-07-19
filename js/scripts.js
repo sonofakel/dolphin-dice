24bee17b54140eb9afa58887a6f13290b64e0799
Andrew Parks, Charlie Kelson and Victor Puentes <charlie@>
2017-07-19 13:18:21 -0700

    lunch commit.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,35 @@
 //Business Logic
+var player1ResultTotal=0;
+var player2ResultTotal=0;
+var playerCycleTotal=0;
 
-
-function ranNum(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
+function ranNum() {
+  return Math.floor(Math.random() * 6) + 1;
 }
-
 
 
 //Front-end logic
 $(function(){
-  $("#dolph-buttons").click(function(event){
+  $(".dolph-roll").click(function(event){
 
-    var random = ranNum(1,6);
+    var random = ranNum();
+
+    var plusCycle = random += playerCycleTotal;
+    var plusPlayer1 = random += player1ResultTotal;
+
+    console.log(plusCycle);
+    console.log(plusPlayer1);
+
+
+
+    // if (random === 1) {
+    //   player1ResultTotal - playerCycleTotal
+    // playerCycleTotal = 0
+    // switch to player 2
+    // }
 
     $("#curr-roll").text(random);
+
 
   })
 });
